9261daeb4b633a9c86f86cff19e2dd192d4eb208
Christina Gin <cgin1211@gmail.com>
2017-06-19 18:04:22 +0000

    added translator function. completed basic tutorial

--- piglatin/views.py
+++ piglatin/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hello(request):
+def home(request):
     return render(request, 'home.html')
 
 def translate(request):
-    return HttpResponse("You're on the translate page")
+    original = request.GET['originaltext'].lower()
+
+    translation = ''
+
+    for word in original.split():
+        if word[0] in ['a', 'e', 'i', 'o', 'u']:
+            translation += word 
+            translation +='yay '
+        else:
+            translation += word[1:]
+            translation += word[0]
+            translation += 'ay '
+    return render(request, 'translate.html', {'original':original, 'translation': translation})
+
+def about(request):
+    return render(request, 'about.html')
